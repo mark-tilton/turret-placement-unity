c30b98451bddd0152105277456b0624e86e17f26
Mark Tilton <insomain@gmail.com>
2018-07-28 22:20:45 -0400

    Add placement widget

---
+++ Assets/Player/Scripts/PlacementWidget.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementWidget : MonoBehaviour
+{
+    public GameObject _widgetPrefab;
+
+	private Transform _widgetInstance = null;
+    private Camera _camera;
+
+	// Use this for initialization
+    void Start()
+    {
+        _camera = transform.GetChild(1).GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(
+            _camera.transform.position,
+            _camera.transform.position + _camera.transform.forward * 100
+        );
+        RaycastHit rayResult;
+        if (Physics.Raycast(
+            _camera.transform.position,
+            _camera.transform.forward,
+            out rayResult
+        ))
+        {
+			ShowWidget();
+			_widgetInstance.position = rayResult.point;
+			_widgetInstance.rotation = new Quaternion(
+				rayResult.normal.x,
+				rayResult.normal.y,
+				rayResult.normal.z,
+				0
+			);
+        }
+		else
+		{
+			HideWidget();
+		}
+    }
+
+	void ShowWidget()
+	{
+		if (_widgetInstance == null)
+		{
+			_widgetInstance = Instantiate(_widgetPrefab).transform;
+		}
+	}
+
+	void HideWidget()
+	{
+		if (_widgetInstance != null)
+		{
+			Destroy(_widgetInstance.gameObject, 0);
+			_widgetInstance = null;
+		}
+	}
+}
